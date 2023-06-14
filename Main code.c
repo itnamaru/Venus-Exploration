192b28e651f3f3e8f3ff1b6a35473a0f8dd35223
Batssss <115535887+Batssss@users.noreply.github.com>
2023-06-14 14:33:47 +0200

    Update Main code.c

--- Main code.c
+++ Main code.c
@@ -1,212 +1,131 @@
-#include <servo.h>
-#define pinIRt 4
-#define pinIRo 7
-servo myservo1;
-servo myservo2;
-servo myservo3;
-servo myservo4;
-enum state {left, right};
+#include <Servo.h>
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+enum state { left,
+             right };
 state var;
-Servo myservo; 
+Servo myservo;
 int pos;
 long duration, cm;
 const int pingPin = 7;
-int valIRt;
-int valIRo;
-int pos = 0;
-void setup()
-{
-    myservo1.attach(12); // right wheel
-    myservo2.attach(13); // left wheel
-    myservo3.attach(11); // Ultrasonic motor
-    myservo4.attach(10); // grabbing motor
-    Serial.begin(9600);  // serial connection for communication
-    int potpin = 0;
-  myservo.attach(10);
+int checking = 0;
+int del = 50;
+int potpin = 0;
+int val1;
+int val2;
+int ramp = 1;
+int go_to_base = 0;
+int direction;
+int sam = 0;
+int froto = 0;
+int vald;
+void setup() {
+
+  Serial.begin(9600);  // serial connection for communication
+
+  int potpin = 0;
+  myservo.attach(11);
   var = left;
-  pos = 60;
+  pos = 0;
 }
 
-void nav_traverse (int direction)
+void nav_traverse(int direction)  // code for navigating the map
 {
-    val1 = analogRead(potpin);
-    val2 = analogRead(potpin);
-    myservo1.attach(12); // right wheel
-    myservo2.attach(13); // left wheel
-    switch (direction)
-    {
-    case '1': // forward
-        val1 = map(val1, 0, 1023, 180, 0);
-        val2 = map(val2, 0, 1023, 0, 180);
-        delay(50);
-        break;
-    case '2': // backward
-        val1 = map(val1, 0, 1023, 0, 180);
-        val2 = map(val2, 0, 1023, 180, 0);
-        delay(50);
-        break;
-    case '3': // right
-        val1 = map(val1, 0, 1023, 180, 0);
-        val2 = map(val2, 0, 1023, 180, 0);
-        delay(500);
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    case '4': // left
-        val1 = map(val1, 0, 1023, 0, 180);
-        val2 = map(val2, 0, 1023, 0, 180);
-        delay(500);
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    case '5': // stop
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    default:
-        return;
-    }
-    myservo1.write(val1);
-    myservo2.write(val2);
-    return 0;
-}
-void nav_grab(){
-    val1 = analogRead(potpin);
-    val2 = analogRead(potpin);
-    myservo1.attach(12); // right wheel
-    myservo2.attach(13); // left wheel
-    switch (direction)
-    {
-    case '1': // forward
-        val1 = map(val1, 0, 1023, 180, 0);
-        val2 = map(val2, 0, 1023, 0, 180);
-        delay(300);
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    case '2': // backward
-        val1 = map(val1, 0, 1023, 0, 180);
-        val2 = map(val2, 0, 1023, 180, 0);
-        delay(300);
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    case '3': // right
-        val1 = map(val1, 0, 1023, 180, 0);
-        val2 = map(val2, 0, 1023, 180, 0);
-        delay(50);
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    case '4': // left
-        val1 = map(val1, 0, 1023, 0, 180);
-        val2 = map(val2, 0, 1023, 0, 180);
-        delay(50);
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    case '5': // stop
-        myservo1.detach();
-        myservo2.detach();
-        break;
-    default:
-        return;
-    }
-    myservo1.write(val1);
-    myservo2.write(val2);
-    return 0;
+  myservo1.attach(12);  // right wheel
+  myservo2.attach(13);  // left wheel
+  val1 = analogRead(potpin);
+  val2 = analogRead(potpin);
+  if (direction == 1)  // forward
+  {
+    val1 = map(val1, 0, 1023, 180, 0);
+    val2 = map(val2, 0, 1023, 0, 180);
+    // delay(50);
+  } else if (direction == 2)  // backward
+  {
+    val1 = map(val1, 0, 1023, 0, 180);
+    val2 = map(val2, 0, 1023, 180, 0);
+    delay(50);
+  } else if (direction == 3)  // right
+  {
+    val1 = map(val1, 0, 1023, 180, 0);
+    val2 = map(val2, 0, 1023, 180, 0);
+    delay(500);
+    myservo1.detach();
+    myservo2.detach();
+  } else if (direction == 4)  // left
+  {
+    val1 = map(val1, 0, 1023, 0, 180);
+    val2 = map(val2, 0, 1023, 0, 180);
+    delay(500);
+    myservo1.detach();
+    myservo2.detach();
+  } else if (direction == 5)  // stop
+  {
+    myservo1.detach();
+    myservo2.detach();
+  } else {
+    return;
+  }
+  myservo1.write(val1);
+  myservo2.write(val2);
+  delay(15);
+  return 0;
 }
 
-void IR()
-{
-    return 0;
-}
+void loop() {
 
-void ultrasonic()
-{
-  switch (var) {
-
-    case left:
+  vald = digitalRead(2);
+  if (var == left) {
     pos += 1;
-    myservo3.write(pos);                   // sets the servo position based on the variable 'pos'
-    delay(15);
-    if (pos ==  120)
-    {
+    myservo.write(pos);
+    // delay(1);
+    if (pos == 90) {
       var = right;
+      // checking = 1;
     }
-    break;
+  }
 
-    case right:
+  if (var == right) {
     pos -= 1;
-    myservo3.write(pos);
-    delay(15);                   // sets the servo position based on the variable 'pos'
-    if (pos ==  60)
-    {
+    myservo.write(pos);
+    // delay(1);
+    if (pos == 30) {
       var = left;
+      // froto = 1;
     }
-    break;
-    
   }
+
   pinMode(pingPin, OUTPUT);
-    digitalWrite(pingPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(pingPin, HIGH);
-    delayMicroseconds(5);
-    digitalWrite(pingPin, LOW);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
 
-    pinMode(pingPin, INPUT);
-    duration = pulseIn(pingPin, HIGH);
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
 
-    cm = duration / 29 / 2;
-    Serial.print(cm);
-    Serial.print("cm");
-    Serial.println();
-}
-void IRt()
-{
-  valIR = digitalRead(pinIR);
-  if (valIR == 0)
-  {
-    // Serial.println("STOP");
-  }
-  else
-  {
-    // Serial.println("GO");
-  }
-}
-void IRt()
-{
-  valIR = digitalRead(pinIRt);
-  if (valIRt == 0)
-  {
-    // Serial.println("STOP");
-  }
-  else
-  {
-    // Serial.println("GO");
-  }
-}
-void IRo()
-{
-  valIR = digitalRead(pinIRo);
-  if (valIRo == 1)
-  {
-//     Serial.println("STOP");
-  }
-  else
-  {
-//     Serial.println("GO");
-  }
-}
-void ramp_sequence()
-{
+  cm = duration / 29 / 2;
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
 
-    return 0;
-}
-void Communicate()
-{
-    
-}
-void loop()
-{
+  if (cm <= 25) {
+    nav_traverse(3);
+    // delay(1000);
+    nav_traverse(5);
+    // sam = 0;
+  }
+
+  else if (vald == 0) {
+    nav_traverse(3);
+    // delay(1000);
+    nav_traverse(5);
+  }
+  else if (vald == 1) {
+    nav_traverse(1);
+    delay(25);
+  }
 }
